75d6fbd3de77f36c82498a4b5c27c3c0c75a1c67
Chris <gatorman7654@gmail.com>
2017-09-10 22:48:30 -0400

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,105 @@
+/*
+----CLIENT SIDE----
+COP4504 - Project 1
+written by - Chris Kile
+Note- port IP is hardcoded below. You will need to change this to your IP. If you google "what is my ip" 
+it will come up
+
+TODO:
+	1. better IP system
+	2. IP verification
+
+*/
+
+
+import java.io.*;
+import java.net.*;
+
+public class Client
+{
+	public static void main(String args[])
+	{
+		while(true)
+		{
+			try
+			{
+				String ip = "73.104.15.60";
+				//setup for user input from console
+				InputStreamReader stdInReader = new InputStreamReader(System.in);
+				BufferedReader stdIn = new BufferedReader(stdInReader);
+				
+				
+				
+				//flag to determine if q or Q for quittin time
+				//boolean quit = false;
+				
+				
+				//make connection
+				System.out.println("Making connection....");
+				Socket socket = new Socket(ip, 9090);
+				System.out.println("Setting up reader/writer...");
+				//setup for socket toServer
+				BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
+				//setup for socket fromServer
+				PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
+				
+				while(true)
+				{
+					String userInput = "";
+					String serverOutput = "";
+					PrintMenu();
+					userInput = stdIn.readLine();
+					toServer.println(userInput);
+					if(userInput.equals("Q")||userInput.equals("q"))
+					{
+						toServer.close();
+						fromServer.close();
+						stdInReader.close();
+						stdIn.close();
+						socket.close();
+						System.exit(0);
+					}else
+					{
+						System.out.println("Sending to server...");
+						//toServer.println(userInput);
+						while(!fromServer.ready())
+						{
+						}
+						System.out.println("Receiving from server...");
+						serverOutput = fromServer.readLine();
+						if(serverOutput.equals("-1"))
+						{
+							System.out.println("Error on input.");
+						}else
+						{
+							System.out.println(serverOutput);
+						}
+						
+					}
+				}
+			}catch(IOException e)
+			{
+				System.out.println(e.toString());
+			}
+		
+		}
+	//end main
+	}
+	
+	public static void PrintMenu()
+	{
+		System.out.println("-----------------------------");
+		System.out.println("A. Date and Time");
+		System.out.println("B. Uptime");
+		System.out.println("C. Memory Use");
+		System.out.println("D. NetStat");
+		System.out.println("E. Current Users");
+		System.out.println("F. Running Processes");
+		System.out.println("Q. Quit");
+		System.out.println("-----------------------------");
+		System.out.println("Input");
+	//end PrintMenu
+	}
+//end client
+}
---
+++ Server.java
@@ -0,0 +1,157 @@
+/*
+----SERVER SIDE----
+COP4504 - Project 1
+written by - Chris Kile
+
+Note- port is hardcoded below as 9090
+
+TODO:
+	1. Netsat
+	2. Current Users
+	3. running processes
+
+*/
+
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.lang.management.*;
+import java.util.concurrent.TimeUnit;
+
+public class Server
+{
+	public static void main(String args[])
+	{
+		int port = 9090;
+		//start of try block for port connection, locks port
+		try
+		{
+			//create ServerSocket to listen on
+			ServerSocket server = new ServerSocket(port);
+			
+			//start of main while loop, should never exit
+			while(true)
+			{
+				String clientInput = "";
+				String clientOutput = "";
+				
+				System.out.println("Listening on port " + port);
+				
+				//accept incoming request
+				Socket socket = server.accept();
+				System.out.println("Client accepted.");
+				
+				System.out.println("Setting up reader/writer...");
+				//setup for Server to Client
+				BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
+				//setup for Client to Server
+				PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+				System.out.println("Data stream established...");
+				System.out.println("Waiting for client input...");
+				
+				//start of try block for input handling
+				try
+				{
+					while((clientInput = fromSocket.readLine()) != null)
+					{
+						System.out.println("Client sent: " + clientInput);
+						clientOutput = ProcessInput(clientInput);
+						if(clientOutput.equals("Q") || clientOutput.equals("q"))
+						{
+							socket.close();
+						}else
+						{
+						System.out.println("Sending: " + clientOutput);
+						toSocket.println(clientOutput);
+						}
+					}
+				}catch(IOException e)
+				{
+					System.out.println("Client disconnected.");
+					socket.close();
+				}
+			}
+		}catch(IOException e)
+		{
+			System.out.println("Error: port " + port + " is not working.");
+		}
+	
+	//end main
+	}
+	
+	public static String UptimeFormat(long uptime)
+	{
+		//converts ms to HH:MM:SS
+		String formattedUptime = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(uptime),
+		TimeUnit.MILLISECONDS.toMinutes(uptime) % TimeUnit.HOURS.toMinutes(1),
+		TimeUnit.MILLISECONDS.toSeconds(uptime) % TimeUnit.MINUTES.toSeconds(1));
+		return formattedUptime;
+	//end UptimeFormat
+	}
+	
+	public static String Memory()
+	{
+		System.out.println("Sending Memory Usage.");
+		MemoryUsage usage = ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage();
+		long memory = usage.getUsed();
+		return (memory%1000 + "kb");
+	//end memory
+	}
+	
+	public static String Uptime()
+	{
+		long uptime;
+		
+		//gets uptime from jvm
+		RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
+		uptime = rb.getUptime();
+		return UptimeFormat(uptime);
+	//end uptime
+	}
+	
+	public static String DateAndTime()
+	{
+		Date date = new Date();
+		return date.toString();
+	//end DateAndTime
+	}
+	
+	public static String ProcessInput(String clientInput)
+	{
+		String output = "";
+		
+		if(clientInput.equals("A") || clientInput.equals("a"))
+		{
+			//output = "A";
+			output = DateAndTime();
+		}else if(clientInput.equals("B") || clientInput.equals("b"))
+		{
+			//output = "B";
+			output = Uptime();
+		}else if(clientInput.equals("C") || clientInput.equals("c"))
+		{
+			//output = "C";
+			output = Memory();
+		}else if(clientInput.equals("D") || clientInput.equals("d"))
+		{
+			output = "D";
+		}else if(clientInput.equals("E") || clientInput.equals("e"))
+		{
+			output = "E";
+		}else if(clientInput.equals("F") || clientInput.equals("f"))
+		{
+			output = "F";
+		}else if(clientInput.equals("Q") || clientInput.equals("q"))
+		{
+			output = "Q";
+		}else
+		{
+			return "-1";
+		}
+		return output;
+	//end ProcessInput
+	}
+//end server
+}
